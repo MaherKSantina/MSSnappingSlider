1373c41baad9f4605584a5ab69e879e2039dfc26
Maher Santina <maher.santina90@gmail.com>
2019-06-10 21:11:58 +1000

    Initial commit

---
+++ Example/MSSnappingSlider/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MSSnappingSlider
+//
+//  Created by MaherKSantina on 06/10/2019.
+//  Copyright (c) 2019 MaherKSantina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
